4d0b4e0d57d6ee7d78a25e202e31ae91517d0244
Quentin Meunier <71881980+quentinM22@users.noreply.github.com>
2022-08-30 20:59:12 +0200

    test2

--- projet1CalculElectro.js
+++ projet1CalculElectro.js
@@ -1,92 +1,130 @@
+let reloadWindow = true;
+
 function verif(saisie) {
   if (isNaN(saisie)) {
     saisie = verif(prompt("Recommence, ce n'est pas un nombre!"));
   }
   return saisie;
 }
-let choose = prompt(
-  // tension tapez(U ou V) / intensité tapez(I) / résitance tapez(R)/
-  "Que voulez-vous calculez ? (puissance tapez(P), rendement(Re))"
-);
-while (
-  //   choose.toUpperCase() != "U" &&
-  //   choose.toUpperCase() != "V" &&
-  //   choose.toUpperCase() != "I" &&
-  choose.toUpperCase() != "RE" &&
-  choose.toUpperCase() != "P"
-) {
-  choose = prompt(
-    // tension tapez(U ou V) / intensité tapez(I) / résitance tapez(R)/
-    "Je n'es pas compris! Que voulez-vous calculez ? (puissance tapez(P), rendement(Re))"
-  );
+function reload(saisie) {
+  while (saisie != "o" && saisie != "n") {
+    prompt("Erreur ... \n Voulez vous continuer à faire des calcul o/n");
+  }
+  if (saisie == "o") {
+    reloadWindow = true;
+  } else {
+    reloadWindow = false;
+    alert("Merci  d'avoir utilisé calculEleec =)");
+  }
 }
-// -----Puissance-------------------
-if (choose.toUpperCase() == "P") {
-  let tension = prompt("Tension Simple ? tapez 'o' pour oui  / 'n' pour non");
 
-  while (tension.toLowerCase() != "o" && tension.toLowerCase() != "n") {
-    tension = prompt(
-      "Je n'es pas compris! Tension Simple (mono ou continue) ? o / n"
-    );
-  }
-
-  if (tension.toLowerCase() == "o") {
-    let U = verif(prompt("Ajouter Tension en Volt"));
-    let I = verif(prompt("Ajouter Intensité en Ampère"));
-    let P = 0;
-    alert(`calcul P=U*I P = ${U}V x ${I}A résultat ${(P = U * I)}W`);
-  } else {
-    let U = verif(prompt("Ajouter tension en Volt"));
-    let I = verif(prompt("Ajouter Intensité en Ampère"));
-    let P = 0;
-    P = U * I * Math.sqrt(3);
-    alert(
-      `calcul P=U*I*\u221a3 -> P = ${U}V x ${I}A x\u221a 3 \n résultat ${Math.round(
-        P
-      )}W`
-    );
-  }
-} else if (choose.toUpperCase() == "RE") {
-  let choosePa = prompt(
-    "connaissez vous la puissance absorbé par le moteur? o/n"
+while (reloadWindow == true) {
+  let choose = prompt(
+    // tension tapez(U ou V) / intensité tapez(I) / résitance tapez(R)/
+    "Que voulez-vous calculez ? (puissance tapez(P), rendement moteur(Re), Tension(U ou V))"
   );
-  while (choosePa.toLowerCase() != "o" && choosePa.toLowerCase() != "n") {
-    prompt("connaissez vous la puissance absorbé par le moteur? o/n");
-  }
-  if (choosePa.toLowerCase() == "o") {
-    let Pa = verif(prompt("Ajouter Puissance Absorbé en Watt"));
-    let Pu = verif(prompt("Ajouter Puissance Utile en Watt"));
-    let eta = Pu / Pa;
-    alert(
-      `calcul \u03b7 = Pu / Pa -> \u03b7= ${Pu}/${Pa} \n résultat ${eta.toFixed(
-        1
-      )}`
+  while (
+    choose.toUpperCase() != "U" &&
+    //   choose.toUpperCase() != "I" &&
+    choose.toUpperCase() != "RE" &&
+    choose.toUpperCase() != "P"
+  ) {
+    choose = prompt(
+      // intensité tapez(I) / résitance tapez(R)/
+      "Je n'es pas compris! Que voulez-vous calculez ? (puissance tapez(P), rendement moteur(Re), Tension(U))"
     );
-  } else {
-    let U = verif(prompt("Ajouter Tension en Volt"));
-    let I = verif(prompt("Ajouter Intensité en Ampère"));
-    let cosPhi = verif(prompt("Ajouter Cos\u03c6 du moteur"));
-    let Pa = Math.round(U * I * Math.sqrt(3) * Math.cos(cosPhi));
-    alert(`Calcul Puissance absorbé Prix en compte`);
+  }
 
-    let Pu = verif(prompt("Ajouter Puissance Utile en Watt"));
-    let eta = Pu / Pa;
-    while (eta == 0 || eta > 1) {
-      U = verif(prompt("Ajouter Tension en Volt"));
-      I = verif(prompt("Ajouter Intensité en Ampère"));
-      cosPhi = verif(prompt("Ajouter Cos\u03c6 du moteur"));
-      Pa = Math.round(U * I * Math.sqrt(3) * Math.cos(cosPhi));
+  if (choose.toUpperCase() == "P") {
+    // -----Puissance-------------------
+    let tension = prompt("Tension Simple ?  oui ou non  'o/n'  ");
+
+    while (tension.toLowerCase() != "o" && tension.toLowerCase() != "n") {
+      tension = prompt(
+        "Je n'es pas compris! Tension Simple (mono ou continue) ? 'o/n'"
+      );
+    }
+
+    if (tension.toLowerCase() == "o") {
+      let U = verif(prompt("Ajouter Tension en Volt"));
+      let I = verif(prompt("Ajouter Intensité en Ampère"));
+      let P = 0;
+      alert(`calcul P=U*I P = ${U}V x ${I}A résultat ${(P = U * I)}W`);
+    } else {
+      let U = verif(prompt("Ajouter tension en Volt"));
+      let I = verif(prompt("Ajouter Intensité en Ampère"));
+      let P = 0;
+      P = U * I * Math.sqrt(3);
+      alert(
+        `calcul P=U*I*\u221a3 -> P = ${U}V x ${I}A x\u221a 3 \n résultat ${Math.round(
+          P
+        )}W`
+      );
+    }
+  } else if (choose.toUpperCase() == "RE") {
+    // ----Rendement
+    let choosePa = prompt(
+      "connaissez vous la puissance absorbé par le moteur? o/n"
+    );
+    while (choosePa.toLowerCase() != "o" && choosePa.toLowerCase() != "n") {
+      prompt("connaissez vous la puissance absorbé par le moteur? o/n");
+    }
+    if (choosePa.toLowerCase() == "o") {
+      let Pa = verif(prompt("Ajouter Puissance Absorbé en Watt"));
+      let Pu = verif(prompt("Ajouter Puissance Utile en Watt"));
+      let eta = Pu / Pa;
+      alert(
+        `calcul \u03b7 = Pu / Pa -> \u03b7= ${Pu}/${Pa} \n résultat ${eta.toFixed(
+          1
+        )}`
+      );
+    } else {
+      let U = verif(prompt("Ajouter Tension en Volt"));
+      let I = verif(prompt("Ajouter Intensité en Ampère"));
+      let cosPhi = verif(prompt("Ajouter Cos\u03c6 du moteur"));
+      let Pa = Math.round(U * I * Math.sqrt(3) * Math.cos(cosPhi));
       alert(`Calcul Puissance absorbé Prix en compte`);
 
-      Pu = verif(prompt("Ajouter Puissance Utile en Watt"));
-      eta = Pu / Pa;
-    }
-    alert(
-      `calcul Pa =  UxIx\u221a3xcos\u03c6 -> Pa = ${U} x ${I} x \u221a3 x cos${cosPhi} \n résultat ${Pa}
+      let Pu = verif(prompt("Ajouter Puissance Utile en Watt"));
+      let eta = Pu / Pa;
+      while (eta == 0 || eta > 1) {
+        U = verif(prompt("Ajouter Tension en Volt"));
+        I = verif(prompt("Ajouter Intensité en Ampère"));
+        cosPhi = verif(prompt("Ajouter Cos\u03c6 du moteur"));
+        Pa = Math.round(U * I * Math.sqrt(3) * Math.cos(cosPhi));
+        alert(`Calcul Puissance absorbé Pris en compte`);
+
+        Pu = verif(prompt("Ajouter Puissance Utile en Watt"));
+        eta = Pu / Pa;
+      }
+      alert(
+        `calcul Pa =  UxIx\u221a3xcos\u03c6 -> Pa = ${U} x ${I} x \u221a3 x cos${cosPhi} \n résultat ${Pa}
       \n
     calcul \u03b7 = Pu / Pa -> \u03b7= ${Pu}/${Pa} \n résultat ${eta.toFixed(
-        1
-      )}`
-    );
+          1
+        )}`
+      );
+    }
+  } else if (choose.toUpperCase() == "U") {
+    // Tension
+
+    let form = prompt("Connaissez vous la Résistance du circuit? o/n");
+    while (form.toLowerCase() != "o" && form.toLowerCase() != "n") {
+      prompt("Connaisser vous la Résistance du circuit? o/n");
+    }
+    if (form.toLowerCase() == "o") {
+      let R = verif(prompt("Ajouter Résistance en Ohm"));
+      let I = verif(prompt("Ajouter l'intensité en Ampère"));
+      let U = R * I;
+      alert(`calcul U = R x I -> U = ${R} x ${I}  résultat ${U.toFixed(1)}V`);
+    } else if (form.toLowerCase() == "n") {
+      let P = verif(prompt("Ajouter Puissance en Watt"));
+      let I = verif(prompt("Ajouter l'intensité en Ampère"));
+      let U = P / I;
+      alert(`calcul U = P x I -> U = ${P} x ${I}  résultat ${U.toFixed(1)}V`);
+    }
   }
+  let reloadQuestion = reload(
+    prompt("Voulez vous continuer à faire des calcul o/n")
+  );
 }
